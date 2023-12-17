194bc25958d2625f9fa6152be79bb5371ca3bf91
mikay <mikay@Mikayla_PC>
2023-12-17 14:49:45 -0700

    Initial commit

---
+++ CSC320_M1CT_Option2/src/M1CT_Option2.java
@@ -0,0 +1,12 @@
+
+public class M1CT_Option2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Restaurant Name: Explorado Market");
+		System.out.println("Bussiness Address: 11 Old Town Square, Unit 121");
+		System.out.println("City, State, Zip Code: Fort Collins, CO, 80524");
+
+	}
+
+}
